920fabea0fbfc00bed772b8b455347d9d4a0680a
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-26 11:16:42 +0530

    2nd of Project 5

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const route = express.Router();
 const userController = require("../controllers/userController");
-const booksController = require("../controllers/booksController");
-const reviewController = require("../controllers/reviewController");
+const productController = require("../controllers/productController");
+const cartController = require("../controllers/cartController");
+// const orderController = require("../controllers/orderController");
 const middleware = require("../middleware/auth")
 const aws = require("../aws/s3")
 
@@ -11,22 +12,6 @@ route.post("/register" , userController.createUser)
 
 route.post("/login" , userController.loginUser)
 
-// route.post("/write-file-aws", aws.uploadBookcover)
 
-route.post("/books" ,middleware.authentication, booksController.createBooks)
-
-route.get("/books" ,middleware.authentication, booksController.getBooks)
-
-route.get("/books/:bookId" ,middleware.authentication,booksController.getBooksbyId)
-
-route.put("/books/:bookId" ,middleware.authentication, booksController.updateBooks)
-
-route.delete("/books/:bookId" ,middleware.authentication, booksController.deleteBooksbyId)
-
-route.post("/books/:bookId/review" , reviewController.createReviews)
-
-route.put("/books/:bookId/review/:reviewId" , reviewController.updateReviews)
-
-route.delete("/books/:bookId/review/:reviewId" , reviewController.deleteReviewbyId)
 
 module.exports = route
