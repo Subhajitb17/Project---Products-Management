c0d62038e12fc933cc3b2385d84430b308a1b9fc
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-26 13:15:06 +0530

    2nd of Project 5

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const route = express.Router();
-const {createUser, loginUser, getUserDeatailsById} = require("../controllers/userController");
+const {createUser, loginUser, getUserDeatailsById, updateUserDetails} = require("../controllers/userController");
 const productController = require("../controllers/productController");
 const cartController = require("../controllers/cartController");
 // const orderController = require("../controllers/orderController");
@@ -13,6 +13,8 @@ route.post("/register" , createUser)
 route.post("/login" , loginUser)
 
 route.get("/user/:userId/profile" , authentication, getUserDeatailsById)
+
+route.put("/user/:userId/profile" , authentication, updateUserDetails)
 
 
 
