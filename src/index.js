3dee6c42e0147691d49a4a3cc2d71c7c92362e15
Saswath1403 <saswathsingh1403@gmail.com>
2022-08-08 17:00:33 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const app = express();
 const multer = require("multer")
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().any());
